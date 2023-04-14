5e11e714f0f024eaf82662666f627045d2216ee4
BragiKS <bks9@hi.is>
2023-04-14 13:18:03 +0000

    bætti við alien 2 og 3

---
+++ src/main/java/hi/verkefni/spaceinvaders/Alien_two.java
@@ -0,0 +1,9 @@
+package hi.verkefni.spaceinvaders;
+
+import javafx.scene.image.ImageView;
+
+public class Alien_two extends ImageView {
+    public Alien_two() {
+        FXML_Lestur.lesa(this, "alien2-view.fxml");
+    }
+}
