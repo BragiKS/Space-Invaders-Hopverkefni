5c0715a331172d658f7a25981752fb5734069614
ThorriThorskur <73035171+ThorriThorskur@users.noreply.github.com>
2023-04-14 12:21:32 +0000

    Add files via upload

--- src/main/java/hi/verkefni/spaceinvaders/SpaceApplication.java
+++ src/main/java/hi/verkefni/spaceinvaders/SpaceApplication.java
@@ -16,7 +16,7 @@ public class SpaceApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(SpaceApplication.class.getResource("playArea-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(),600, 700);
+        Scene scene = new Scene(fxmlLoader.load(),800, 750);
         SpaceController sc = fxmlLoader.getController();
         //sc.controls();
         //sc.start();
