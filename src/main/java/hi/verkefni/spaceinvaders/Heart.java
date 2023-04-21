794fbfcd34fd24da3c779e2aaf9cfce25ef45761
ThorriThorskur <sigthorri2001@gmail.com>
2023-04-21 11:13:55 +0000

    Heart

---
+++ src/main/java/hi/verkefni/spaceinvaders/Heart.java
@@ -0,0 +1,9 @@
+package hi.verkefni.spaceinvaders;
+
+import javafx.scene.image.ImageView;
+
+public class Heart extends ImageView {
+    public Heart() {
+        FXML_Lestur.lesa(this, "Heart.fxml");
+    }
+}
